0b2eb8783b82105cbcb9508557e89ceb0150acfa
fangtianq <fangtianq@users.noreply.github.com>
2019-11-20 16:28:07 +0800

    Update Chapter10Application.java

--- chapter10/src/main/java/com/battcn/Chapter10Application.java
+++ chapter10/src/main/java/com/battcn/Chapter10Application.java
@@ -12,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Chapter10Application {
 
     public static void main(String[] args) {
+        System.out.println("gggggg===");
         SpringApplication.run(Chapter10Application.class, args);
     }
 
